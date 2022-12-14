0d4f053a0d871125e3d2c9b8b0b5299648a737c7
Gordon Bailey <gb@gordonbailey.net>
2022-12-14 03:08:45 +0000

    .clang-format: Increase max line length
    Also format 2022/2/main.cc

--- 2022/2/main.cc
+++ 2022/2/main.cc
@@ -20,8 +20,7 @@ const auto only_element = [](auto &&r) {
   const auto length = ranges::distance(r);
 
   if (length != 1) {
-    throw std::runtime_error(
-        fmt::format("Length should be 1 but is {}", length));
+    throw std::runtime_error(fmt::format("Length should be 1 but is {}", length));
   }
 
   return *r.begin();
@@ -31,20 +30,22 @@ int main(int argc, char *argv[]) {
   const std::string contents = gb::read_file(argv[1]);
   std::cout << "Total size is " << contents.size() << std::endl;
 
-  const auto scores = contents | ranges::views::split('\n') |
-                      ranges::views::transform([](auto &&r) {
-                        return r | ranges::views::split(' ') |
-                               ranges::views::transform(only_element) |
-                               ranges::views::transform(move_from_char) |
-                               gb::to_tuple<2>;
-                      }) |
-                      ranges::views::transform([](auto &&moves) {
-                        return std::apply(points, moves);
-                      });
+  {
+    const auto scores = contents | ranges::views::split('\n') |             //
+                        ranges::views::transform([](auto &&r) {             //
+                          return r | ranges::views::split(' ') |            //
+                                 ranges::views::transform(only_element) |   //
+                                 ranges::views::transform(move_from_char) | //
+                                 gb::to_tuple<2>;
+                        }) |                                        //
+                        ranges::views::transform([](auto &&moves) { //
+                          return std::apply(points, moves);
+                        });
 
-  const auto result = ranges::accumulate(scores, 0);
+    const auto result = ranges::accumulate(scores, 0);
 
-  fmt::print("The total score when following the strategy is {}\n", result);
+    fmt::print("The total score when following the strategy is {}\n", result);
+  }
 
   return 0;
 }
